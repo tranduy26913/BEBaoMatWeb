ad33d66c242e466602472359ed0474f8e99b3f12
tranduy <tranbaoduy4@gmail.com>
2022-04-27 20:30:49 +0700

    fix csp, X-Content-Type-Options Header, hsts

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
 import {UserRoute,AuthRoute, NovelRoute, CommentRoute} from './routers/index.js'
+import helmet from 'helmet'
 
 dotenv.config()
 
@@ -15,9 +16,24 @@ const URI=process.env.MONGODB_URL;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}))
-app.use(cors({ credentials: true, origin:"https://tranduy26913.github.io"}));
+app.use(cors({ credentials: true, origin:"https://tranduy26913.github.io"}));//fix lỗi cross-domain
 app.use(cookieParser());
-app.disable('x-powered-by');
+app.disable('x-powered-by');//fix lỗi leak info from x-powered-by
+app.use(helmet.frameguard())//fix lỗi clickjacking
+app.use(helmet.noSniff());//fix lỗi X-Content-Type-Options Header Missing
+
+app.use(helmet.hsts()); // default configuration
+app.use(helmet.hsts("<max-age>", "<includeSubdomains>"));
+
+const csp = require('helmet-csp')
+app.use(csp({
+   directives: {
+       defaultSrc: ["'self'"],  // default value for all directives that are absent
+       scriptSrc: ["'self'"],   // helps prevent XSS attacks
+       frameAncestors: ["'none'"],  // helps prevent Clickjacking attacks
+       styleSrc: ["'none'"]
+    }
+}))
 
 mongoose.connect(URI)
     .then(()=>{
