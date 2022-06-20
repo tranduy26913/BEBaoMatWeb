fe21b1040ebc3002f306ec554feb3fdb8f859a5f
tranduy <tranbaoduy4@gmail.com>
2022-06-20 18:37:30 +0700

    update cache

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ app.use(
     })
   );
 
+  app.use(function (req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next()
+});
 
 mongoose.connect(URI)
     .then(()=>{
